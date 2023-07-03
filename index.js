17a8da8a1b046e4e92d6e96f25f64b1f81ddd1ad
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-07-02 20:05:48 -0700

    creates inner functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+ return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+saturdayFun("read a book");
+
+function mondayWork(activity="go to the office"){
+ return `This Monday, I will ${activity}.`
+}
+mondayWork();
+mondayWork("work from home")
+
+function wrapAdjective(wraper="*"){
+    return function (adjective="special"){
+    return `You are ${wraper}${adjective}${wraper}!` 
+};}
+wrapAdjective("*")("a hard worker");
